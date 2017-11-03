2d03923ca15b654dc2206e036fc1889ec5facd0e
Gaëtan DUHAMEL <gaetan.duhamel@plante-et-cite.fr>
2017-11-03 13:57:24 +0100

    Setting react-instantsearch

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,3 +1,14 @@
+import {
+  Highlight,
+  Hits,
+  InstantSearch,
+  Pagination,
+  RefinementList,
+  SearchBox,
+  SortBy,
+  Stats
+} from "react-instantsearch/dom";
+
 import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
@@ -7,10 +18,63 @@ const mapStateToProps = state => ({
   user: state.auth.user.user
 });
 
+const Hit = ({ hit }) => (
+  <div className="hit">
+    <div className="hit-image">
+      <img src={hit.cover_sm} alt={hit.objectID} />
+    </div>
+    <div className="hit-content">
+      <Highlight attributeName="taxon" hit={hit} />
+      <Highlight attributeName="nom" hit={hit} />
+    </div>
+  </div>
+);
+
+const Sidebar = () => (
+  <div className="sidebar">
+    <h5>Catégorie</h5>
+    <RefinementList attributeName="category" />
+    <h5>Écorce</h5>
+    <RefinementList attributeName="bark" withSearchBox />
+  </div>
+);
+
+const Content = () => (
+  <div className="content">
+    <div className="info">
+      <Stats />
+      <SortBy
+        defaultRefinement="vegebasePlantes"
+        items={[
+          { label: "Les plus documentées", value: "vegebasePlantes" },
+          { label: "A-Z", value: "vegebasePlante_taxonDesc" }
+        ]}
+      />
+    </div>
+    <Hits hitComponent={Hit} />
+    <div className="pagination">
+      <Pagination showLast />
+    </div>
+  </div>
+);
+
 class Search extends React.Component {
+  state = {
+    query: ""
+  };
   componentDidMount() {
     console.log(this.props);
+    this.setState({
+      query: new URLSearchParams(this.props.location.search).get("q")
+    });
   }
+
+  changeQuery = q => {
+    console.log("changing query");
+    this.setState({ query: q });
+    return this.state.query;
+  };
+
   render() {
     return (
       <div>
@@ -23,6 +87,25 @@ class Search extends React.Component {
             <b>{new URLSearchParams(this.props.location.search).get("q")}</b>
           </p>
         </div>
+        <InstantSearch
+          appId="YUJNYEHBTI"
+          apiKey="7e8a0d4ae30b28542ada596b547a0dc8"
+          indexName="vegebasePlantes"
+          searchState={{
+            query: this.state.query
+          }}
+          onSearchStateChange={value => this.changeQuery(value)}
+        >
+          <header>
+            <SearchBox
+              translations={{ placeholder: "Rerchercher des plantes..." }}
+            />
+          </header>
+          <main>
+            <Sidebar />
+            <Content />
+          </main>
+        </InstantSearch>
       </div>
     );
   }
