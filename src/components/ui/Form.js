d86b1212eee88a3417218da70d181cfca46b33b7
Gaëtan DUHAMEL <gaetan.duhamel@plante-et-cite.fr>
2017-11-03 09:51:51 +0100

    Extracting CustomInput

---
+++ src/components/ui/Form.js
@@ -0,0 +1,65 @@
+import styled, { css } from "react-emotion";
+
+import React from "react";
+
+const InputWrapper = styled("div")`
+  height: 40px;
+  position: relative;
+  background: #f1f1f1;
+  border-radius: 3px;
+  border: 1px solid #f1f1f1;
+  margin: 10px;
+  padding-left: 40px;
+`;
+const InputLogo = styled("span")`
+  height: 40px;
+  position: absolute;
+  width: 40px;
+  padding: 10px;
+  color: #171a1f;
+`;
+
+const InputArea = styled("input")`
+  font: 300 16px "Proxima Nova", sans-serif;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 0;
+  border-width: 0;
+  background: white;
+  padding: 12px;
+  position: relative;
+  height: 38px;
+  border: 0;
+  right: 0;
+  font-size: 13px;
+  border-radius: 0 2px 2px 0;
+  color: rgba(0, 0, 0, 0.87);
+  & :focus {
+    outline: none;
+  }
+`;
+
+export const CustomInput = ({
+  input,
+  placeholder,
+  label,
+  type,
+  logo,
+  meta: { touched, error }
+}) => (
+  <InputWrapper>
+    <span>
+      <img
+        src={logo}
+        alt="email"
+        style={{
+          position: "absolute",
+          fontSize: 12,
+          top: 10,
+          left: 10
+        }}
+      />
+    </span>
+    <InputArea {...input} type={type} placeholder={placeholder} />
+  </InputWrapper>
+);
--- src/containers/auth/AuthForm.js
+++ src/containers/auth/AuthForm.js
@@ -2,6 +2,7 @@ import { Field, reduxForm } from "redux-form";
 import React, { Component } from "react";
 import styled, { css } from "react-emotion";
 
+import { CustomInput } from "../../components/ui/Form";
 import email from "../../assets/images/email.svg";
 import lock from "../../assets/images/lock.svg";
 
@@ -30,68 +31,6 @@ const AuthTabs = styled("div")`
   margin-bottom: 10px;
 `;
 
-const InputWrapper = styled("div")`
-  height: 40px;
-  position: relative;
-  background: #f1f1f1;
-  border-radius: 3px;
-  border: 1px solid #f1f1f1;
-  margin: 10px;
-  padding-left: 40px;
-`;
-const InputLogo = styled("span")`
-  height: 40px;
-  position: absolute;
-  width: 40px;
-  padding: 10px;
-  color: #171a1f;
-`;
-
-const InputArea = styled("input")`
-  font: 300 16px "Proxima Nova", sans-serif;
-  box-sizing: border-box;
-  width: 100%;
-  padding: 0;
-  border-width: 0;
-  background: white;
-  padding: 12px;
-  position: relative;
-  height: 38px;
-  border: 0;
-  right: 0;
-  font-size: 13px;
-  border-radius: 0 2px 2px 0;
-  color: rgba(0, 0, 0, 0.87);
-  & :focus {
-    outline: none;
-  }
-`;
-
-const CustomInput = ({
-  input,
-  placeholder,
-  label,
-  type,
-  logo,
-  meta: { touched, error }
-}) => (
-  <InputWrapper>
-    <span>
-      <img
-        src={logo}
-        alt="email"
-        style={{
-          position: "absolute",
-          fontSize: 12,
-          top: 10,
-          left: 10
-        }}
-      />
-    </span>
-    <InputArea {...input} type={type} placeholder={placeholder} />
-  </InputWrapper>
-);
-
 let AuthForm = props => {
   const { handleSubmit } = props;
   console.log(props);
@@ -104,7 +43,7 @@ let AuthForm = props => {
         />
         <div>Floriscope</div>
       </FormHeader>
-      <AuthTabs>Auth Tabs</AuthTabs>
+      <AuthTabs>auth-tabs</AuthTabs>
       <Field
         name="email"
         placeholder="Adresse électronique"
