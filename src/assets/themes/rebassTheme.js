d78dc09297a5df83a591c855c3b20b13818b462e
Gaëtan DUHAMEL <gaetan.duhamel@plante-et-cite.fr>
2017-11-10 10:52:10 +0100

    Add AdminHome Links

--- src/assets/themes/rebassTheme.js
+++ src/assets/themes/rebassTheme.js
@@ -5,12 +5,8 @@ export default {
   space: [0, 4, 8, 16, 32, 64, 128],
   fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72, 96, 142, 200],
   weights: [300, 400, 600, 700],
-  colors: {
-    black: "#000",
-    white: "#fff",
-    ...colors
-  },
   radius: 4,
+  radii: [2, 4],
   font:
     '"Proxima Nova", "Brandon Text", Helvetica,-apple-system, BlinkMacSystemFont, sans-serif',
   monospace: '"SF Mono", "Roboto Mono", Menlo, monospace'
--- src/containers/Admin.js
+++ src/containers/Admin.js
@@ -1,22 +1,93 @@
-import { Card, Container } from "rebass-emotion";
+import { Box, Button, Card, Container, Flex, Text } from "rebass-emotion";
 import React, { Component } from "react";
+import { borderRadius, color, fontSize, space, width } from "styled-system";
 import styled, { css } from "react-emotion";
+
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 const AdminHomeContainer = styled("div")`
   height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
+
+const StyledLink = styled(Link)`
+  height: auto;
+  text-transform: uppercase;
+  font: "Brandon Grotesque", "Brandon Text", Helvetica, sans-serif;
+  ${space} ${color} ${borderRadius} ${width};
+`;
+
+const Caption = styled(Text)`
+  font-family: "Authenia", sans-serif;
+  font-weight: 500;
+  letter-spacing: 6px;
+  color: rgba(256, 256, 256, 1);
+  ${fontSize};
+`;
+
+const AdminLink = ({ link, title, color }) => (
+  <StyledLink
+    to={link}
+    width={[1, 1 / 2, 1 / 3]}
+    m={2}
+    color="white"
+    bg={color}
+    p={3}
+    borderRadius={1}
+  >
+    <Flex mx={-2}>
+      <Box w={9 / 10} px={2}>
+        <Text p={1} color="white" bold>
+          {title}
+        </Text>
+      </Box>
+      <Box w={1 / 10} px={2}>
+        <Text p={1} bold color="white">
+          >
+        </Text>
+      </Box>
+    </Flex>
+  </StyledLink>
+);
+
+// Salut Typhaine
 
 class Admin extends Component {
   render() {
     return (
       <AdminHomeContainer>
-        <Card>Admin Area</Card>
+        <Caption
+          center
+          fontSize={[5, 6, 8]}
+          bold
+          children="Le changement c'est maintenant!"
+        />
+        <AdminLink
+          link="/admin/collections"
+          title="Gestion des collections"
+          color="green"
+        />
+        <AdminLink
+          link="/admin/collections"
+          title="Gestion de la photothèque"
+          color="yellow"
+        />
+        <AdminLink
+          link="/admin/collections"
+          title="Gestion des plantes"
+          color="red"
+        />
       </AdminHomeContainer>
     );
   }
 }
 
-export default Admin;
+const mapStateToProps = state => ({
+  location: state.routing.location
+});
+
+export default connect(mapStateToProps, null)(Admin);
