c4845c80fab2c83e6206fd8feb710d0b3f9363d5
Gaëtan DUHAMEL <gaetan.duhamel@plante-et-cite.fr>
2018-01-19 15:37:46 +0100

    Add more page transitions...

--- src/App.js
+++ src/App.js
@@ -77,6 +77,40 @@ const AdminRoute = ({
   );
 };
 
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `scale(${styles.scale})`
+  };
+}
+
+// wrap the `spring` helper to use a bouncy config
+function bounce(val) {
+  return spring(val, {
+    stiffness: 330,
+    damping: 22
+  });
+}
+
+// child matches will...
+const bounceTransition = {
+  // start in a transparent, upscaled state
+  atEnter: {
+    opacity: 0,
+    scale: 1.2
+  },
+  // leave in a transparent, downscaled state
+  atLeave: {
+    opacity: bounce(0),
+    scale: bounce(0.8)
+  },
+  // and rest at an opaque, normally-scaled state
+  atActive: {
+    opacity: bounce(1),
+    scale: bounce(1)
+  }
+};
+
 const switchRule = css`
   position: relative;
   & > div {
@@ -155,7 +189,13 @@ class App extends React.Component {
     return (
       <Provider theme={theme}>
         <div className="App">
-          <Switch location={this.props.location}>
+          <AnimatedSwitch
+            atEnter={bounceTransition.atEnter}
+            atLeave={bounceTransition.atLeave}
+            atActive={bounceTransition.atActive}
+            mapStyles={mapStyles}
+            className="route-wrapper"
+          >
             <Route exact path="/" component={Home} />
             <Route path="/authentification" component={Authentification} />
             <Route path="/fonctionnalites" component={Features} />
@@ -244,7 +284,7 @@ class App extends React.Component {
 
             <Route path="/unauthorized" component={Unauthorized} />
             <Route component={NotFound} />
-          </Switch>
+          </AnimatedSwitch>
         </div>
       </Provider>
     );
